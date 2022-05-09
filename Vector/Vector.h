c78be233cac3ab103afe4573a5b481e20b79447f
DeepIsland <zhaojiacheng1@kingsoft.com>
2022-05-09 20:22:56 +0800

    feat:增加Vector的构造/析构函数

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -7,12 +7,29 @@ public:
 	typedef T* iterator;
 
 public:
-	Vector() {
-
+	Vector() { data = nullptr; size = 0; capacity = 0;}
+	Vector(int vectorLength) {
+		data = new T[vectorLength];
+		size = vectorLength;
+		capacity = vectorLength;
+	}
+	Vector(const Vector& vector) {
+		size = vector.size;
+		capacity = vector.capacity;
+		data = new value_type[capacity];
+		for (int i = 0; i < size; ++i) {
+			data[i] = vec.data[i];
+		}
+	}
+	~Vector() {
+		delete[] data;
+		data = nullptr;
+		size = 0;
+		capacity = 0;
 	}
 
 private:
-	value_type* _data;
-	size_t _size;
-	size_t _capacity;	//vector自动扩容
+	value_type* data;
+	size_t size;
+	size_t capacity;	//vector自动扩容
 };
