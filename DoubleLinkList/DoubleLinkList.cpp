f5f0c28a368e767ccd1b3d4e78ce66eb74badf09
DeepIsland <zhaojiacheng1@kingsoft.com>
2022-05-16 17:56:05 +0800

    feat:[DoubleLinkList] 双向链表实现提交

--- DoubleLinkList/DoubleLinkList.cpp
+++ DoubleLinkList/DoubleLinkList.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main() {
-
+	DoubleLinkList<int> list1;
+	//auto list1 = new DoubleLinkList();	//¶î Ê²Ã´Çé¿ö£¡
+	auto node1 = list1.push(1);
+	auto node2 = list1.push(3);
+	//auto node3 = new Node<int>(3);
+	auto node3 = list1.insert(node1, 3);
+	auto ret = list1.find(2);
+	//list1.remove(node2);
+	list1.popAll();
+	return 0;
 }
--- DoubleLinkList/DoubleLinkList.h
+++ DoubleLinkList/DoubleLinkList.h
@@ -1,12 +1,136 @@
 #include <iostream>
 
-typedef struct DoubleLinkListNode {
-	struct DLNode *pLast;
-	struct DLNode *pNext;
-	int data;
-}DLNode, *DLinkList;
+template <class T>
+class DoubleLinkList;
 
-template<typename T>
+template <class T>
+class Node {
+public:
+	friend class DoubleLinkList<T>;
+
+	T data;
+	Node<T>* pLast;
+	Node<T>* pNext;
+public:
+	Node(const T& data) {
+		this->data = data;
+		this->pLast = nullptr;
+		this->pNext = nullptr;
+	}
+};
+
+template <class T>
 class DoubleLinkList {
+public:
+	DoubleLinkList() {
+		this->pHead = nullptr;
+		this->length = 0;
+	}
 
+	~DoubleLinkList() {
+		Node<T>* pTemp = this->pHead;
+		Node<T>* pNode = pTemp;
+		for (; pTemp != nullptr && pTemp->pNext != nullptr;)
+		{
+			pTemp = pTemp->pNext;
+			delete pNode;
+			pNode = pTemp;
+		}
+
+	}
+
+	bool isEmpty() {
+		if (this->pHead == nullptr) return true;
+		return false;
+	}
+
+	int listLength() {
+		return this->length;
+	}
+
+	Node<T>* push(const T& data) {
+		if (this->isEmpty()) {
+			this->pHead = new Node<T>(data);
+			this->length++;
+			return this->pHead;
+		}
+		else {
+			Node<T>* pTemp = this->pHead;
+			while (pTemp->pNext != nullptr) {
+				pTemp = pTemp->pNext;
+			}
+			pTemp->pNext = new Node<T>(data);
+			pTemp->pNext->pLast = pTemp;
+			this->length++;
+			return pTemp->pNext;
+		}
+	}
+
+	Node<T>* insert(Node<T>* curNode, const int& index) {
+		//没有的话直接创建新的
+		if (this->isEmpty() || index > this->listLength()) {
+			this->push(curNode->data);
+			return this->pHead;
+		}
+
+		Node<T>* pTemp = this->pHead;
+		int tempIndex = index;
+		while (--tempIndex > 0) {
+			pTemp = pTemp->pNext;
+		}
+		curNode->pNext = pTemp->pNext;
+		curNode->pLast = pTemp;
+		if (pTemp->pNext != nullptr) {
+			pTemp->pNext->pLast = curNode;
+		}
+		pTemp->pNext = curNode;
+
+		return this->pHead;
+	}
+
+	Node<T>* find(const int& index) {
+		int curIndex = index;
+		Node<T>* pTemp = this->pHead;
+
+		while (--curIndex > 0) {
+			pTemp = pTemp->pNext;
+		}
+
+		return pTemp;
+	}
+
+	void remove(const Node<T>* curNode) {
+		for (Node<T>* pTemp = this->pHead; pTemp->pNext != nullptr; pTemp = pTemp->pNext)
+			if (pTemp == curNode)
+			{
+				if (pTemp->pNext == nullptr) {
+
+				}
+				Node<T>* pNode = pTemp;
+				pNode->pNext->pLast = pNode->pLast;
+				pNode->pLast->pNext = pNode->pNext;
+				pNode->pLast = nullptr;
+				pNode->pNext = nullptr;
+				delete pTemp;
+				delete pNode;
+				break;
+			}
+	}
+
+	void popAll() {
+		Node<T>* pTemp = this->pHead;
+		Node<T>* pNode;
+		pNode = pTemp;
+		while (pTemp != nullptr) {
+			pTemp = pTemp->pNext;
+			delete pNode;
+			pNode = pTemp;
+		}
+
+		this->length = 0;
+		this->pHead = nullptr;
+	}
+private:
+	Node<T>* pHead;
+	int length;
 };
